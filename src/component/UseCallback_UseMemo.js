e46acf3d49aefa9b2592004c76f164ae7128c61b
joy-adhikary <joyadhikary074@gmail.com>
2023-02-25 21:52:53 +0600

    Modified

--- src/component/UseCallback_UseMemo.js
+++ src/component/UseCallback_UseMemo.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useMemo } from "react";
 import Title from "./title.js";
 import ShowCount from "./showCount.js";
 import Button from "./Button.js";
@@ -52,14 +52,46 @@ function UseCallback_UseMemo() {
 
 
    const incrementByFive = useCallback(() => {
-     setCount2((prevCount2) => prevCount2 + 1);
+     setCount2((prevCount2) => prevCount2 + 5);
    }, []);
+
+
+  //  ! UseMemo 
+
+
+
+  // * let say amader ei function ta just count1 er jonno calculate korte hobe kintu eita akta function so joto bar eita render korbe toto bar e ei function ta run korbe 
+  // ! amra ei jaigai usecallback use korty parbo nah karon amader to function ta ke mone rekhe lav nai karon mone rakhlau amake sei run e korty hocche bar bar 
+  // ? so ei jonno amra useMemo use korty pari . jeita ei value take mone kore rakhbe . r eitar dependency arrary te just jader state change korle eita calculate korty hobe tder value set kore dibo 
+
+  // const IsEven =()=>{
+    
+  //   // ! protibar amake ei complex calculation ta korte hocche . 
+    
+  //   let x=0;
+  //   while(x<100000000)x++;
+
+  //   return count1%2===0?"Even":"Odd";
+  // }
+
+  // ? so eitake amra bar bar run nah kore just amra mone kore rakhbo 
+
+   const IsEven = useMemo(() => {
+
+    // ! akn just count1 er value change korle eita ei calculation ta korbe else ager mone rakha value e print kore dibe 
+    
+    let x = 0;
+     while (x < 100000000) x++;
+    console.log("Only run when count1 is tiggered")
+     return count1 % 2 === 0 ? "Even" : "Odd";
+   },[count1]);
 
 
   return (
     <div className="App">
       <Title />
       <ShowCount count={count1} title="Counter 1" />
+      <span>{IsEven}</span>
       <Button handleClick={incrementByOne} text="Increment By one">
         Increment By one
       </Button>
